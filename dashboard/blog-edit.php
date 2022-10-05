db6144dccee00418dcd0afe50c29fbf803c3e9a4
YahyaLateef <m.yahya.lateef@gmail.com>
2022-10-05 10:17:07 +0500

    work of image enter incomplete

--- dashboard/blog-edit.php
+++ dashboard/blog-edit.php
@@ -1,5 +1,12 @@
 <?php
-include('include/db_connect.php');
+// connect to the database
+$conn = mysqli_connect('localhost', 'yahya', '1234', 'blog-table');
+
+// check connection
+if(!$conn){
+    echo 'Connection error: '. mysqli_connect_error();
+}
+
 if(isset($_POST['delete'])){
     $id_to_delete = mysqli_real_escape_string($conn,$_POST['id_to_delete']);
     $sql = "DELETE  FROM blogs WHERE id = $id_to_delete";
@@ -14,9 +21,9 @@ if(isset($_POST['delete'])){
 //check
 if(isset($_GET['id'])){
 $id = mysqli_real_escape_string($conn,$_GET['id']);
-$sql = "SELECT * FROM blogs WHERE id = $id";
+$sql = "SELECT images,content,created_at FROM blogs WHERE id = $id";
 $result = mysqli_query($conn,$sql);
-$blog = mysqli_fetch_assoc($result);
+$blogs = mysqli_fetch_assoc($result);
 mysqli_free_result($result);
 mysqli_close($conn);
 }
@@ -27,21 +34,12 @@ mysqli_close($conn);
 <html>
 <?php include('include/header.php')?>
 <div class="container center grey-text">
-    <?php if($blog):?>
-        <h4><?php echo htmlspecialchars($blog['username']); ?></h4>
-        <p><?php echo htmlspecialchars($blog['email']);?></p>
-        <p><?php echo date($blog['created_at']);  ?></p>
-        <p><?php echo htmlspecialchars($blog['content']); ?></p>
-<!-- Delete form -->
+   <img src="">
 <form action="blog-edit.php" method="POST">
-    <input type="hidden" name="id_to_delete" value="<?php  echo ($blog['id']); ?>"></input>
+    <input type="hidden" name="id_to_delete" value="<?php  echo ($blogs['user_id']); ?>"></input>
     <input type="submit" name="delete" value="delete" class="btn brand z-depth-0"></input>
 
 <form>
-
-    <?php else: ?>
-        <h5>No such pizza exists!</h5>
-    <?php endif  ?>     
 </div>
 
 <?php include('include/footer.php')?>
